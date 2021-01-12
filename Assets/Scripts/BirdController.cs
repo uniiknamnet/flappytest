296dfce646bbc82a19f8117491916a08e6c0f3bc
Tommi <btowa001@edu.xamk.fi>
2021-01-12 17:42:32 +0200

    Assets

---
+++ Assets/Scripts/BirdController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    public int speed = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+    }
+}
